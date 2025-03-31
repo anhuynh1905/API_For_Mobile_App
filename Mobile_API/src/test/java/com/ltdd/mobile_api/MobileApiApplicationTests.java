4569cebd4afcf9e25983b5def7713c3f23c37dae
an <22162001@student.hcmute.edu.vn>
2025-03-31 12:43:36 +0700

    azure database connection and create user, model entities

---
+++ Mobile_API/src/test/java/com/ltdd/mobile_api/MobileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltdd.mobile_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
